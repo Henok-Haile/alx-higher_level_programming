484f863f2c9b5162fb336bec4c53099640dc4203
Henok-Haile <henokhaile53@gmail.com>
2023-05-08 12:02:49 -0700

    Task 11 Advanced

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,3 +2,12 @@
 import sys
 sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
+'''
+import sys
+
+def print_to_stderr(message):
+    sys.stderr.write(message + '\n')
+
+print_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.exit(1)
+'''
