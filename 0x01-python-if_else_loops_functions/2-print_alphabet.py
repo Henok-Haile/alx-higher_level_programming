8cd480ee1a3b21b7a1c0bf7829bdccf6877fbfc7
Henok-Haile <henokhaile53@gmail.com>
2023-05-09 12:23:37 -0700

    Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for alphabets in range(ord('a'), ord('z') + 1):
-    print(chr(alphabets), end="")
+for letter in range(ord('a'), ord('z') + 1):
+    print(f"{chr(letter)}", end="")
