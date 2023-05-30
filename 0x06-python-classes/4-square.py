eec11363bb2db8b4bd8ef243ba50c5cc5baa87d6
Henok-Haile <henokhaile53@gmail.com>
2023-05-30 14:25:37 -0700

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""create a class Square."""
+
+
+class Square:
+    """A Class Square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size: size of the created square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """retrive the size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns current area of the square."""
+        return (self.__size ** 2)
