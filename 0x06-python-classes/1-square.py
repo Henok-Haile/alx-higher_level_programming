31b6701ec9ffe785a1a5390984866b96e31d5a56
Henok-Haile <henokhaile53@gmail.com>
2023-05-30 13:49:45 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""create a class Square"""
+
+
+class Square:
+    """A class Square"""
+
+    def __init__(self, size):
+        """Initializes a new Square.
+
+
+        Args:
+        size: size of a the square initalized.
+        """
+        self.__size = size
