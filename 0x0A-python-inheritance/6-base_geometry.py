81c04826ed5acb10a074dc3e09dc65893341cfcc
Henok-Haile <henokhaile53@gmail.com>
2023-06-12 14:05:54 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        """unimplemented."""
+        raise Exception("area() is not implemented")
