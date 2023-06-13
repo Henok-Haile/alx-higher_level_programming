fa0a6e26a6a6a6932922ad04ab28f3c0e206882f
Henok-Haile <henokhaile53@gmail.com>
2023-06-13 05:36:24 -0700

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""Defines a fucntion that reads a text file"""
+
+
+def read_file(filename=""):
+    """prints the contents of a utf8 text file to the stdout."""
+
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
+
