8f57aed0d92c988a57a003629870decff34226e0
Henok-Haile <henokhaile53@gmail.com>
2023-09-30 13:04:49 -0700

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,5 +7,9 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == []:
         return None
+    
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
     else:
         return max(list_of_integers)
