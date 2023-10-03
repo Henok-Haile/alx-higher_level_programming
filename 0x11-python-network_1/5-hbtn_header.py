c2901cf7852b6da87cb93d1cbec56f2d76d2339c
Henok-Haile <henokhaile53@gmail.com>
2023-10-03 08:20:52 -0700

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    try:
+        res = requests.get(sys.argv[1])
+        print(res.headers['X-Request-Id'])
+    except Exception:
+        pass
