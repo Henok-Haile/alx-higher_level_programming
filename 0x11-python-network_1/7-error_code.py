5ac8920aa04cedc30b1890c6f969d042a79f19a3
Henok-Haile <henokhaile53@gmail.com>
2023-10-03 08:26:35 -0700

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print('Error code:', res.status_code)
+    else:
+        print(res.text)
